4482419aa7dd0674d73dd1300409d9563048e973
Hosea Kandie <zeddarn@gmail.com>
2017-03-07 20:02:50 +0300

    Updated Allocations

--- src/pages/mark/post.confirm.ts
+++ src/pages/mark/post.confirm.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {NavController, Platform, ToastController, Events, ViewController} from 'ionic-angular';
+import {Component, OnInit, ElementRef} from '@angular/core';
+import {NavController, Platform, ToastController, Events, ViewController, NavParams} from 'ionic-angular';
 import {FormGroup, FormArray, FormBuilder, } from '@angular/forms';
 import {Storage} from '@ionic/storage';
 import {AlertController} from 'ionic-angular';
@@ -7,68 +7,136 @@ import {ViewChild} from '@angular/core';
 import {Content} from 'ionic-angular';
 import {AuthService} from '../../providers/auth-service';
 import {Contacts} from 'ionic-native';
+import {Geolocation} from 'ionic-native';
+import {IonicGallery} from '../../app/ionic-gallery';
+declare var google;
+
 @Component({
     selector: 'page-post.confirm',
-    templateUrl: 'post.confirm.html'
+    templateUrl: 'post.confirm.html',
+    providers: [IonicGallery]
 })
 export class PostConfirm implements OnInit {
-    public myForm: FormGroup;
     @ViewChild(Content) content: Content;
     user_id: string;
     createSuccess = false;
     contacts: any;
     contactsfound: any;
-    hidden_contacts:any;
-    constructor(public viewCtrl: ViewController, private events: Events, private toastCtrl: ToastController, public navCtrl: NavController, public alertCtrl: AlertController, public storage: Storage, private platform: Platform, private auth: AuthService, private _fb: FormBuilder) {
+    hidden_contacts: any;
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+    post_data: any;
+    mark_images: any = [];
+    mark_address: string;
+    mark_long: string;
+    mark_lat: string;
+    mark_contacts: string;
+    mark_message: string;
+    mark_as_group: string;
+    mark_suprise: string;
+    mark_notify: string;
+    selectedItem: any;
+    icons: string[];
+    items: Array<any>;
+    options: any;
+    constructor(public params: NavParams, public viewCtrl: ViewController, private events: Events, private toastCtrl: ToastController, public navCtrl: NavController, public alertCtrl: AlertController, public storage: Storage, private platform: Platform, private auth: AuthService, private _fb: FormBuilder) {
+        this.selectedItem = params.get('item');
+
+        this.options = {
+            urlKey: 'URL',
+            thumbKey: 'thumb',
+            titleKey: 'title',
+            contentKey: 'note',
+            colWidth: 120,
+            thumbnailClickAction: this.thumbnailClick,
+            actionClass: this,
+            viewActionButtons: [{
+                icon: 'download',
+                action: this.save
+            },
+            {
+                icon: 'heart',
+                action: this.like
+            },
+            ]
+        }
+
         this.platform.ready().then(() => {
+            this.post_data = params.get('post_data');
+            console.log(this.post_data);
+            this.mark_images = this.post_data.mark_images;
+            this.mark_address = this.post_data.mark_address;
+            this.mark_long = this.post_data.mark_long;
+            this.mark_lat = this.post_data.mark_lat;
+            this.mark_contacts = this.post_data.mark_contacts;
+            this.mark_message = this.post_data.mark_message;
+            this.mark_as_group = this.post_data.mark_as_group;
+            this.mark_suprise = this.post_data.mark_suprise;
+            this.mark_notify = this.post_data.mark_notify;
+
+            this.loadMap();
             this.auth.getUserInfo().subscribe(succ => {
-                let info = succ;
-                this.user_id = info.user_id;
             });
         });
-
+        this.items = [];
+        for (let i = 1; i < 20; i++) {
+            let s = {
+                title: 'Item ' + i,
+                note: 'This is item #' + i,
+                thumb: 'http://placehold.it/120X120',
+                URL: `http://placehold.it/${i}00X300`,
+            }
+            this.items.push(s);
+        }
     }
     ngOnInit(): void {
 
     }
 
     public save_post(model) {
-        var hide_me_from_contacts = (this.myForm.controls['hide_me_from_contacts'].value);
-        var is_contact_hidden = (this.myForm.controls['is_contact_hidden'].value);
-        let credentials = {user_id: this.user_id, is_contact_hidden: is_contact_hidden, hide_me_from_contacts: hide_me_from_contacts}
-        this.auth.save_update_location_wise_settings(credentials).subscribe(success => {
-            if (success) {
-                this.createSuccess = true;
-                this.showPopup("Success", "Location wise settings updated.");
-                this.viewCtrl.dismiss();
-            } else {
-
-            }
-        },
-            error => {
-                this.showPopup("Error", error);
-            });
 
 
     }
-    public fetch_update_location_wise_settings() {
-        let credentials = {user_id: this.user_id}
-        this.auth.fetch_update_location_wise_settings(credentials).subscribe(data => {
-            let is_contact_hidden = data.is_contact_hidden;
-            this.hidden_contacts = data.contacts;
-            console.log(this.hidden_contacts);
-            if (data.success == '1') {
-                this.myForm.controls['is_contact_hidden'].setValue('1' == is_contact_hidden ? true : false);
-                let contacts = data.contacts;
+    public addInfoWindow(marker, content) {
+        let infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
 
-            } else {
+        google.maps.event.addListener(marker, 'click', () => {
+            infoWindow.open(this.map, marker);
+        });
 
+    }
+    public addMarker() {
+
+        let marker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: this.map.getCenter()
+        });
+
+
+        let content = "<h4>" + this.post_data.mark_address + "!</h4>";
+
+        this.addInfoWindow(marker, content);
+
+    }
+    public loadMap() {
+        let options = {maximumAge: 0, timeout: 10000, enableHighAccuracy: true};
+        Geolocation.getCurrentPosition(options).then((position) => {
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            let mapOptions = {
+                center: latLng, zoom: 15, mapTypeId: google.maps.MapTypeId.ROADMAP
             }
-        },
-            error => {
-                this.showPopup("Error", error);
-            });
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+            this.addMarker();
 
+
+        }, (err) => {
+            console.log(err);
+        });
+    }
+    public fetch_update_location_wise_settings() {
 
     }
     public showPopup(title, text) {
@@ -116,5 +184,31 @@ export class PostConfirm implements OnInit {
         } catch (e) {
             alert("Error:" + e);
         }
+    }
+    save(item) {
+        console.log("in ListPage save()", item, this);
+        this.presentToast(`item ${item.title} saved!`)
+    }
+    thumbnailClick(item) {
+        console.log("in ListPage thumbnailClick()", item, this);
+        this.presentToast(`item ${item} clicked!`)
+    }
+    like(item) {
+        console.log("in ListPage like()", item, this);
+        this.presentToast(`item ${item.title} liked!`)
+
+    }
+    presentToast(text) {
+        let toast = this.toastCtrl.create({
+            message: 'User was added successfully',
+            duration: 3000,
+            position: 'top'
+        });
+
+        toast.onDidDismiss(() => {
+            console.log('Dismissed toast');
+        });
+
+        toast.present();
     }
 }
